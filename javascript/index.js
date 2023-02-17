2537e3b98491294cb1eb77a729754f288f8660cf
gleicebsouza <gleicebsouza@outlook.com>
2023-02-17 16:32:17 -0300

    test conf

--- javascript/index.js
+++ javascript/index.js
@@ -2,6 +2,7 @@ let theme = document.querySelector('body');
 let buttonTheme = document.querySelector('.switch-theme button');
 
 
+//add toggle dark mode
 function SwitchTheme(){
   buttonTheme.addEventListener('click',()=>{
     theme.classList.toggle('light')
